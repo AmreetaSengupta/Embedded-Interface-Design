d6b6cc0afbd93cc847bc59bd98ab6a7d020eca57
ridhishah179 <ridhi.shah@colorado.edu>
2019-12-11 06:19:09 +0000

    initial

--- Super Project/magic_wand_server.py
+++ Super Project/magic_wand_server.py
@@ -5,6 +5,8 @@
 #******************************************************************************************************************
 
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QLabel, QApplication, QMainWindow
+from PyQt5.QtGui import QPixmap
 import sys
 import boto3
 import os
@@ -23,7 +25,7 @@ host_name = "localhost"
 user_name = "eid19"
 passwd_name = "eid19"
 database_name = "EID"
-table_name = "MagicWand_EID"
+table_name = "MagicWand_eid"
 table_name_label = "MagicWand_Label"
 
 s3client.download_file('magic-wand','img1.jpg','./img1.jpg')
@@ -51,11 +53,11 @@ mydb = mysql.connector.connect(
 
 #CREATING A TABLE IN THE DATABASE
 mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE IF NOT EXISTS {} (id INT AUTO_INCREMENT PRIMARY KEY, Command VARCHAR(30))".format(table_name))
+mycursor.execute("CREATE TABLE IF NOT EXISTS {} (id INT AUTO_INCREMENT PRIMARY KEY, Command VARCHAR(150))".format(table_name))
 
 #CREATING A TABLE IN THE DATABASE
 mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE IF NOT EXISTS {} (id INT AUTO_INCREMENT PRIMARY KEY, Label VARCHAR(30))".format(table_name_label))
+mycursor.execute("CREATE TABLE IF NOT EXISTS {} (id INT AUTO_INCREMENT PRIMARY KEY, Label VARCHAR(150))".format(table_name_label))
 
 #DELETING ALL ROWS OF THE TABLE
 mycursor = mydb.cursor()
@@ -73,10 +75,10 @@ response = sqs_client.get_queue_attributes(
     ]
 )
 num_msg=(response['Attributes']['ApproximateNumberOfMessages'])
-a=int(num_msg)
-print(a)
+total_num_msg=int(num_msg)
+print("Total number of commands = {}".format(total_num_msg))
 
-for i in range(a):
+for i in range(total_num_msg):
         q_msg = sqs_client.receive_message(
             QueueUrl='https://sqs.us-east-1.amazonaws.com/229856064192/SuperProject_Cmd',
             MaxNumberOfMessages = 1
@@ -89,6 +91,7 @@ for i in range(a):
                         QueueUrl='https://sqs.us-east-1.amazonaws.com/229856064192/SuperProject_Cmd',
                         ReceiptHandle=rec_handle
                         )
+         
   
 
 #TO VIEW THE CONTENTS OF THE TABLE
@@ -97,28 +100,41 @@ for i in mycursor:
   print(i)
   
 mycursor = mydb.cursor()
-mycursor.execute("SELECT Command FROM MagicWand_EID")
+mycursor.execute("SELECT Command FROM MagicWand_eid")
 myresult = mycursor.fetchall()
 
 Img_corr_cnt = 0
-total = 0
+total_cnt = 0
+img_wrong = 0
+
+print(myresult);
 
 for x in myresult:
-  print(x)
   if(x[0]=="Image correct"):
     Img_corr_cnt = Img_corr_cnt + 1
-  #total = total+1
 
-print(Img_corr_cnt)
-print(a)
+print("Number of Correct Images are {}".format(Img_corr_cnt))
 
-if(Img_corr_cnt != 0):
-  percent_corr = (Img_corr_cnt/a)*100
+for y in myresult:
+  if(y[0]=="Image Identified"):
+    total_cnt = total_cnt + 1
+ 
+print("Total Images are {}".format(total_cnt))
+
+img_wrong = total_cnt - Img_corr_cnt
+
+print("Number of Incorrect Images are {}" .format(img_wrong))
+
+if(total_cnt != 0):
+  percent_corr = (Img_corr_cnt/total_cnt)*100
   correct=str(percent_corr)
-  print('Percentage of Correct Images is' + correct + '%') 
-
+  print('Percentage of Correct Images is ' + correct + '%') 
+else:
+  print('No Commands in queue');
+  
+  
+  
 #######LABEL###############
-
 response_label = sqs_client.get_queue_attributes(
     QueueUrl='https://sqs.us-east-1.amazonaws.com/229856064192/Superproject_Queue',
     AttributeNames=[
@@ -127,7 +143,7 @@ response_label = sqs_client.get_queue_attributes(
 )
 num_msg_label=(response_label['Attributes']['ApproximateNumberOfMessages'])
 b=int(num_msg_label)
-print(b)
+print("Total number of messages in Label Queue are {}".format(b))
 
 for i in range(b):
         q_msg_label = sqs_client.receive_message(
@@ -155,18 +171,6 @@ for l in mycursor:
 
 print(last_label)
 
-#mycursor = mydb.cursor()
-#mycursor.execute("SELECT Label FROM MagicWand_label")
-#myresult_label = mycursor.fetchall()
-
-#for y in myresult_label:
-  #print(y)
-
-#mycursor = mydb.cursor()
-#res=mycursor.execute("SELECT Label FROM MagicWand_EID ORDER BY ID DESC LIMIT 1")
-#print(res)
-
-
 class MainWindow(QtWidgets.QMainWindow):
     
     def __init__(self):
@@ -183,17 +187,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.total_cmd = self.findChild(QtWidgets.QLabel, 'total_cmd')
         self.per_image = self.findChild(QtWidgets.QLabel, 'per_image')
         self.per_cmd = self.findChild(QtWidgets.QLabel, 'per_cmd')
+        self.image = self.findChild(QtWidgets.QLabel, 'Image')
+        self.image.setPixmap(QPixmap('img1.jpg'))
         self.show()
     
     def refresh(self):
         self.image_label.setText("{}".format("IMAGE LABEL NOT FOUND"))
-        self.correct_image.setText("{}".format("0"))
-        self.incorrect_image.setText("{}".format("0"))
-        self.total_image.setText("{}".format("0"))
+        self.correct_image.setText("{}".format(Img_corr_cnt))
+        self.incorrect_image.setText("{}".format(img_wrong))
+        self.total_image.setText("{}".format(total_cnt))
         self.correct_cmd.setText("{}".format("0"))
         self.incorrect_cmd.setText("{}".format("0"))
         self.total_cmd.setText("{}".format("0"))
-        self.per_image.setText("{}".format("0"))
+        self.per_image.setText("{}".format(correct))
         self.per_cmd.setText("{}".format("0"))
 
 if __name__ == "__main__":
